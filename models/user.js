b589419bb9b2c1e801d5c80dc32889ecdf65bb3e
Youssef Doss <youssefdoss97@gmail.com>
2023-01-09 18:47:48 -0500

    through step 3

--- models/user.js
+++ models/user.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const bcrypt = require('bcrypt');
+const db = require('../db');
+const { BCRYPT_WORK_FACTOR } = require('../config');
+const { NotFoundError } = require('../expressError');
+
 /** User of the site. */
 
 class User {
@@ -9,22 +14,67 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    const result = await db.query(
+      `INSERT INTO users (
+        username,
+        password,
+        first_name,
+        last_name,
+        phone,
+        join_at,
+        last_login_at
+      )
+        VALUES ($1, $2, $3, $4, $5, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+        RETURNING username, password, first_name, last_name, phone`,
+      [username, hashedPassword, first_name, last_name, phone],
+    );
+
+    return result.rows[0];
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+    const result = await db.query(
+      `SELECT password
+        FROM users
+        WHERE username = $1`,
+        [username]
+    );
+    const user = result.rows[0];
+
+    return user && await bcrypt.compare(password, user.password) === true;
   }
 
   /** Update last_login_at for user */
 
   static async updateLoginTimestamp(username) {
+    const result = await db.query(
+      `UPDATE users
+        SET last_login_at = CURRENT_TIMESTAMP
+          WHERE username = $1
+          RETURNING username`,
+          [username]
+    );
+    const user = result.rows[0];
+
+    if (!user) throw new NotFoundError(`User ${username} not found`);
   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
+    const result = await db.query(
+      `SELECT username,
+              first_name,
+              last_name
+        FROM users
+        ORDER BY username`
+    );
+
+    return result.rows;
   }
 
   /** Get: get user by username
@@ -37,6 +87,22 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
+    const result = await db.query(
+      `SELECT username,
+              first_name,
+              last_name,
+              phone,
+              join_at,
+              last_login_at
+        FROM users
+        WHERE username = $1`,
+        [username]
+    );
+    const user = result.rows[0];
+
+    if (!user) throw new NotFoundError(`User ${username} not found`);
+
+    return user;
   }
 
   /** Return messages from this user.
@@ -48,6 +114,36 @@ class User {
    */
 
   static async messagesFrom(username) {
+    const result = await db.query(
+      `SELECT m.id,
+              m.to_username,
+              m.body,
+              m.sent_at,
+              m.read_at,
+              u.first_name,
+              u.last_name,
+              u.phone
+        FROM messages AS m
+          JOIN users AS u
+            ON m.to_username = u.username
+        WHERE from_username = $1`,
+        [username]
+    );
+
+    return result.rows.map((m) => {
+      return {
+        id: m.id,
+        to_user: {
+          username: m.to_username,
+          first_name: m.first_name,
+          last_name: m.last_name,
+          phone: m.phone
+        },
+        body: m.body,
+        sent_at: m.sent_at,
+        read_at: m.read_at
+      }
+    });
   }
 
   /** Return messages to this user.
@@ -59,6 +155,36 @@ class User {
    */
 
   static async messagesTo(username) {
+    const result = await db.query(
+      `SELECT m.id,
+              m.from_username,
+              m.body,
+              m.sent_at,
+              m.read_at,
+              u.first_name,
+              u.last_name,
+              u.phone
+        FROM messages AS m
+          JOIN users AS u
+            ON m.from_username = u.username
+        WHERE to_username = $1`,
+        [username]
+    );
+
+    return result.rows.map((m) => {
+      return {
+        id: m.id,
+        from_user: {
+          username: m.from_username,
+          first_name: m.first_name,
+          last_name: m.last_name,
+          phone: m.phone
+        },
+        body: m.body,
+        sent_at: m.sent_at,
+        read_at: m.read_at
+      }
+    });
   }
 }
 
--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,43 @@
 
 const Router = require("express").Router;
 const router = new Router();
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+const { SECRET_KEY } = require('../config');
+const { BadRequestError, UnauthorizedError } = require('../expressError');
+
+const EMPTY_REQUEST_MSG = 'Must pass in a body'
 
 /** POST /login: {username, password} => {token} */
 
+router.post('/login', async function (req, res) {
+  if (req.body === undefined) {
+    throw new BadRequestError(EMPTY_REQUEST_MSG);
+  }
+  const { username, password } = req.body;
+  if (await User.authenticate(username, password)) {
+    User.updateLoginTimestamp(username);
+    const token = jwt.sign({ username }, SECRET_KEY);
+    return res.json({ token });
+  } else {
+    throw new UnauthorizedError('Invalid user/password');
+  }
+});
 
 /** POST /register: registers, logs in, and returns token.
  *
  * {username, password, first_name, last_name, phone} => {token}.
  */
+
+router.post("/register", async function(req, res) {
+  if (req.body === undefined) {
+    throw new BadRequestError(EMPTY_REQUEST_MSG);
+  }
+  const { username } = await User.register(req.body);
+  User.updateLoginTimestamp(username);
+  const token = jwt.sign({ username }, SECRET_KEY);
+  return res.json({ token });
+});
 
 module.exports = router;
